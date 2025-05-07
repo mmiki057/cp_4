c56c03eeed9d333675a9083c921084a7e8847122
en-A007-16 <en-A007-16@CAMPUS.UEK.KRAKOW.PL>
2025-05-07 12:52:15 +0200

    05.07.2025 class

--- src/main/java/pl/iashchenko/productcatalog/SqlProductStorage.java
+++ src/main/java/pl/iashchenko/productcatalog/SqlProductStorage.java
@@ -3,18 +3,60 @@ package pl.iashchenko.productcatalog;
 import java.util.List;
 
 public class SqlProductStorage implements ProductStorage {
+
     @Override
     public List<Product> allProducts() {
-        return null;
+        var sql = "select * from 'product_catalog__products' where id = ?";
+
+        var result = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{productId},
+                (rs, i) -> {
+                    var product = new Product(
+                            UUID.fromString(ts.getString("ID")),
+                            rs.getString("NAME"),
+                            rs.getString("DESCRIPTION")
+                    );
+
+                    return product;
+                });
+        return result;
     }
 
     @Override
     public void save(Product newProduct) {
 
+        var insertSql = """
+                INSERT INTO 'product_catalog__products' (id, name, description)
+                VALUES
+                    (:id, :name, :desc)
+                """;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", newProduct.getId());
+        params.put("desc", newProduct.getDescription());
+        params.put("name", newProduct.getName);
+
     }
 
     @Override
-    public Product loadProductById(String productId) {
-        return null;
+    public List<Product> allProducts() {
+        var sql = "select * from 'product_catalog__products'";
+
+        var result = jdbcTemplate.queryForObject(
+                sql,
+                new Object[]{productId},
+                (rs, i) -> {
+                    var product = new Product(
+                            UUID.fromString(ts.getString("ID")),
+                            rs.getString("NAME"),
+                            rs.getString("DESCRIPTION")
+                    );
+
+                    return product;
+                });
+        return result;
     }
+
+
 }
--- src/test/java/pl/iashchenko/productcatalog/SqlProductStorageTest.java
+++ src/test/java/pl/iashchenko/productcatalog/SqlProductStorageTest.java
@@ -6,8 +6,66 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class SqlProductStorageTest {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    void setupDatabase(){
+        jdbcTemplate.execute("DROP TABLE 'product_catalog__products' IF EXISTS");
+
+        var sql = """
+                CREATE TABLE 'product_catalog__products' (
+                    id VARCHAR(100) NOT NULL,
+                    name VARCHAR(100) NOT NULL,
+                    description VARCHAR(100) NOT NULL,
+                    price DECIMAL (12, 2),
+                    cover VARCHAR(100),
+                    PRIMARY KEY(id)                    
+                """;
+
+        jdbcTemplate.execute(sql);
+    }
+    @Test
+    void itAllowsToInsertIntoTableWithArguments() {
+
+        var insertSql = """
+                INSERT INTO 'product_catalog__products' (id, name, description)
+                VALUES
+                    (?, ?, ?)
+                """;
+
+        jdbcTemplate.update(insertSql, "e9c1fd22-8286-4f7c-b506-79cc2d5310fd", "product1", "nice one");
+
+        var result = jdbcTemplate.queryFotObject("select count(*) from 'product_catalog_products'", Integer.class);
+
+        assert result == 1;
+
+    }
+
+    @Test
+    void itAllowsToInsertIntoTableWithArgumentsNamedParameters() {
+
+        var insertSql = """
+                INSERT INTO 'product_catalog__products' (id, name, description)
+                VALUES
+                    (:id, :name, :desc)
+                """;
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("id", "e9c1fd22-8286-4f7c-b506-79cc2d5310fd");
+        params.put("desc", "products");
+        params.put("name", "nice product");
+
+        var namedJdbc = new NamedParameterJdbcTemplate(jdbcTemplate);
+        namedJdbc.update(insertSql, params);
+
+        var result = jdbcTemplate.queryForObject("select count(*) from 'product_catalog__catalogs'", Integer.class)
+
+        assert result == 1;
+
+    }
     @Test
     void itSaveAndLoadProduct() {
         var product = thereIsProduct();
