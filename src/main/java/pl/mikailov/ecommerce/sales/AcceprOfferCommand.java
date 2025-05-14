232c8ce81a8cff4583160572541bb74c991efbd2
s-A007-16 <s-A007-16@CAMPUS.UEK.KRAKOW.PL>
2025-05-14 12:41:42 +0200

    14.05.2025 class

---
+++ src/main/java/pl/mikailov/ecommerce/sales/AcceprOfferCommand.java
@@ -0,0 +1,7 @@
+package pl.mikailov.ecommerce.sales;
+
+public class AcceprOfferCommand {
+    String fmane;
+    String lname;
+    String email;
+}
---
+++ src/main/java/pl/mikailov/ecommerce/sales/SalesFacade.java
@@ -0,0 +1,9 @@
+package pl.mikailov.ecommerce.sales;
+
+public class SalesFacade {
+    public void addToCart(String productId) {}
+
+    public void acceptOffer(AcceprOfferCommand acceptOffer){}
+
+    public void makeReservationPaid(String reservationId) {}
+}
